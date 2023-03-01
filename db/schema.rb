c81887e67a50d0c25b9db93fa3fde6744c07e9c3
masashi-prum <masashisato.prum@gmail.com>
2023-03-01 11:54:12 +0900

    add password_confirmation column and remove icon column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_27_162751) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_023934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,10 +19,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_27_162751) do
     t.string "full_name"
     t.string "email"
     t.string "password"
-    t.binary "icon"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "self_introduction"
+    t.string "password_confirmation"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
