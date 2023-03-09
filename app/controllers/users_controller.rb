bb884d830cbd755a421b3f60e601be96f8ccbaa0
masashi-prum <masashisato.prum@gmail.com>
2023-03-10 07:48:27 +0900

    Remove password_confirmation from user create page @hiroro-d

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,6 +22,6 @@ class UsersController < ApplicationController
    private
 
     def user_params
-      params.require(:user).permit(:user_name, :full_name, :email, :image, :password, :pssword_confirmation,)
+      params.require(:user).permit(:user_name, :full_name, :email, :image, :password,)
     end
 end
